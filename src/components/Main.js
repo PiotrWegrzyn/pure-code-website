fefffcd622d054cdc84526ac3be850f5ca6db029
piotr-wegrzyn <piotr.wegrzyn@instream.io>
2021-03-07 19:50:57 +0100

    Add article context.

--- src/components/Main.js
+++ src/components/Main.js
@@ -96,13 +96,72 @@ class Main extends React.Component {
             <img src={pic03} alt="" />
           </span>
           <p>
-            Lorem ipsum dolor sit amet, consectetur et adipiscing elit. Praesent
-            eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam
-            erat volutpat. Praesent urna nisi, fringila lorem et vehicula
-            lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices.
-            Aliquam libero et malesuada fames ac ante ipsum primis in faucibus.
-            Cras viverra ligula sit amet ex mollis mattis lorem ipsum dolor sit
-            amet.
+            We are a group of programmers who live by the Al Sweigart's motto "Automate the boring stuff".
+          </p>
+          <p>
+            The amount of human potential waisted on tedious and boring processes is what keeps us awake at night.
+            We know that automation frees people from the burden of performing repetitive tasks but also error rate
+            while increasing efficiency and scalability.
+            With this in mind our missions became to help as many companies as possible to unlock their potential and
+            help them take their business to another level with our solutions.
+          </p>
+          {close}
+        </article>
+
+        <article
+          id="web_scraping"
+          className={`${this.props.article === 'web_scraping' ? 'active' : ''} ${
+            this.props.articleTimeout ? 'timeout' : ''
+          }`}
+          style={{ display: 'none' }}
+        >
+          <h2 className="major">Web Applications</h2>
+          <span className="image main">
+            <img src={pic03} alt="" />
+          </span>
+          <p>
+            Many say information is the new gold and is this paradigm The Web would be the gold mine. This is why we
+            build web scrapers.
+          </p>
+          <p>
+            We are helping companies collect publicly available data from The Web by building python based web scrapers
+            that provide them with the information they need. Whether you're working with AI or make your business on
+            collecting scattered data we would be glad to provide you with our service.
+          </p>
+          {close}
+        </article>
+
+        <article
+          id="web_apps"
+          className={`${this.props.article === 'web_apps' ? 'active' : ''} ${
+            this.props.articleTimeout ? 'timeout' : ''
+          }`}
+          style={{ display: 'none' }}
+        >
+          <h2 className="major">About</h2>
+          <span className="image main">
+            <img src={pic03} alt="" />
+          </span>
+          <p>
+            So far we had the opportunity to build web applications that aid the day to day tasks in financial
+            institutions, hotels and even hospitals.
+          </p>
+          {close}
+        </article>
+
+        <article
+          id="system_integrations"
+          className={`${this.props.article === 'system_integrations' ? 'active' : ''} ${
+            this.props.articleTimeout ? 'timeout' : ''
+          }`}
+          style={{ display: 'none' }}
+        >
+          <h2 className="major">About</h2>
+          <span className="image main">
+            <img src={pic03} alt="" />
+          </span>
+          <p>
+            We integrate systems.
           </p>
           {close}
         </article>
