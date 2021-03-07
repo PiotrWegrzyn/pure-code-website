5a9dd62d3ddd8e1b1e6051f4350175268df27ed9
piotr-wegrzyn <piotr.wegrzyn@instream.io>
2021-03-07 22:47:48 +0100

    Add lotus logo.

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import React from 'react'
 const Header = props => (
   <header id="header" style={props.timeout ? { display: 'none' } : {}}>
     <div className="logo">
-      <span className="icon fa-diamond"></span>
+      <img src="icons/lotus.svg" alt="Pure Code logo" height="69%" style={{margin: '0.8em', filter: "invert(100%)"}}/>
     </div>
     <div className="content">
       <div className="inner">
