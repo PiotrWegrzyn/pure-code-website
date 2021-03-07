d4c66ec0b5298b823666b5dc2aa4e7d2122a6cf7
piotr-wegrzyn <piotr.wegrzyn@instream.io>
2021-03-07 19:53:32 +0100

    Remove form and icons from contact.

--- src/components/Main.js
+++ src/components/Main.js
@@ -113,29 +113,6 @@ class Main extends React.Component {
         >
           <h2 className="major">Contact</h2>
           <h3 className="fa-bold">Email:</h3>
-
-          {/*<form method="post" action="#">*/}
-          {/*  <div className="field half first">*/}
-          {/*    <label htmlFor="name">Name</label>*/}
-          {/*    <input type="text" name="name" id="name" />*/}
-          {/*  </div>*/}
-          {/*  <div className="field half">*/}
-          {/*    <label htmlFor="email">Email</label>*/}
-          {/*    <input type="text" name="email" id="email" />*/}
-          {/*  </div>*/}
-          {/*  <div className="field">*/}
-          {/*    <label htmlFor="message">Message</label>*/}
-          {/*    <textarea name="message" id="message" rows="4"></textarea>*/}
-          {/*  </div>*/}
-          {/*  <ul className="actions">*/}
-          {/*    <li>*/}
-          {/*      <input type="submit" value="Send Message" className="special" />*/}
-          {/*    </li>*/}
-          {/*    <li>*/}
-          {/*      <input type="reset" value="Reset" />*/}
-          {/*    </li>*/}
-          {/*  </ul>*/}
-          {/*</form>*/}
           <ul className="icons">
             <li>
               <a
@@ -145,24 +122,6 @@ class Main extends React.Component {
                 <span className="label">LinkedIn</span>
               </a>
             </li>
-            {/*<li>*/}
-            {/*  <a href="https://codebushi.com" className="icon fa-facebook">*/}
-            {/*    <span className="label">Facebook</span>*/}
-            {/*  </a>*/}
-            {/*</li>*/}
-            {/*<li>*/}
-            {/*  <a href="https://codebushi.com" className="icon fa-instagram">*/}
-            {/*    <span className="label">Instagram</span>*/}
-            {/*  </a>*/}
-            {/*</li>*/}
-            {/*<li>*/}
-            {/*  <a*/}
-            {/*    href="https://github.com/codebushi/gatsby-starter-dimension"*/}
-            {/*    className="icon fa-github"*/}
-            {/*  >*/}
-            {/*    <span className="label">GitHub</span>*/}
-            {/*  </a>*/}
-            {/*</li>*/}
           </ul>
           {close}
         </article>
